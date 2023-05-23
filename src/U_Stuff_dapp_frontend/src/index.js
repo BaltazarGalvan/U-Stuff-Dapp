3a2162725bbfdda01dd8aaae2648fd337230810c
BaltazarGalvan <b_galvan@yahoo.com>
2023-05-23 13:46:10 -0600

    code added & .ic-assets content-security-policy for bootstrap & pinterest images

--- src/U_Stuff_dapp_frontend/src/index.js
+++ src/U_Stuff_dapp_frontend/src/index.js
@@ -1,19 +1,65 @@
 import { U_Stuff_dapp_backend } from "../../declarations/U_Stuff_dapp_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const carouselIndicators = document.querySelector(".carousel-indicators");
+const carouselInner = document.querySelector(".carousel-inner");
+const offCanvasCollapse = document.querySelector(".offcanvas-collapse");
+import imgArray from "./imgArray.js";
 
-  const name = document.getElementById("name").value.toString();
+console.log("imgArray");
 
-  button.setAttribute("disabled", true);
+let carouselInnerHTML = "";
+let carouselIndicatorsHTML = "";
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await U_Stuff_dapp_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+document.querySelector("#navbarSideCollapse").addEventListener("click", () => {
+  offCanvasCollapse.classList.toggle("open");
 });
+
+document.querySelector(".navbar-nav").addEventListener("click", () => {
+  if (offCanvasCollapse.classList.contains("open"))
+    offCanvasCollapse.classList.toggle("open");
+});
+
+imgArray.forEach((carouselImg, index) => {
+  carouselIndicatorsHTML =
+    carouselIndicatorsHTML +
+    `<button
+          style="background-color:white"
+          type="button"
+          data-bs-target="#main-carousel"
+          data-bs-slide-to="${index}"
+          aria-label="Slide ${index + 1}"
+          class="${carouselImg.buttonClass}"
+          ${carouselImg.buttonAriaCurrent}
+        ></button>  `;
+
+  carouselInnerHTML =
+    carouselInnerHTML +
+    `<div class="${carouselImg.class}">
+          <img src="${carouselImg.source}" alt="${carouselImg.altText}"/>
+          <div class="carousel-caption text-start">
+            <h4>${carouselImg.name}</h4>
+            <p>${carouselImg.description}</p>
+          </div>
+        </div>`;
+});
+
+carouselIndicators.innerHTML = carouselIndicatorsHTML;
+carouselInner.innerHTML = carouselInnerHTML;
+
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
+
+//   const name = document.getElementById("name").value.toString();
+
+//   button.setAttribute("disabled", true);
+
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await U_Stuff_dapp_backend.greet(name);
+
+//   button.removeAttribute("disabled");
+
+//   document.getElementById("greeting").innerText = greeting;
+
+//   return false;
+// });
